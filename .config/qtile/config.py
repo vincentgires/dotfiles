5b9e21ab2ce4783c14b1386c1d22f7867090be48
Vincent Girès <mail@vincentgires.com>
2020-11-24 01:31:57 +0100

    Refactor keys assignation

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,93 +4,103 @@ from libqtile.config import Click, Drag, Group, Key, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+keys = []
 mod = 'mod4'
 alt = 'mod1'
 # terminal = guess_terminal()
 terminal = 'st -f hack:size=12'
 
-keys = [
-    # Switch between windows in current stack pane
-    Key([mod], 's', lazy.layout.down(),
-        desc='Move focus down in stack pane'),
-    Key([mod], 'r', lazy.layout.up(),
-        desc='Move focus up in stack pane'),
-    Key([mod], 't', lazy.layout.left(),
-        desc='Move focus left in stack pane'),
-    Key([mod], 'n', lazy.layout.right(),
-        desc='Move focus right in stack pane'),
 
-    # Move windows up or down in current stack
-    Key([mod, 'shift'], 's', lazy.layout.shuffle_down(),
-        desc='Move window down in current stack '),
-    Key([mod, 'shift'], 'r', lazy.layout.shuffle_up(),
-        desc='Move window up in current stack '),
-    Key([mod, 'shift'], 't', lazy.layout.shuffle_left(),
-        desc='Move window left in current stack '),
-    Key([mod, 'shift'], 'n', lazy.layout.shuffle_right(),
-        desc='Move window right in current stack '),
+def assign_multiple_keys(keys, modifiers, key, *commands, desc=''):
+    for k in key:
+        keys.append(Key(modifiers, k, *commands, desc=desc))
 
-    Key([mod], 'd',
-        lazy.layout.shrink(),
-        lazy.layout.decrease_nmaster(),
-        desc=(
-            'Shrink window (MonadTall), '
-            'decrease number in master pane (Tile)')),
-    Key([mod], 'l',
-        lazy.layout.grow(),
-        lazy.layout.increase_nmaster(),
-        desc=(
-            'Expand window (MonadTall), '
-            'increase number in master pane (Tile)')),
 
-    Key([mod], 'v', lazy.layout.maximize(), desc='Maximize'),
-    Key([mod], 'f', lazy.window.toggle_fullscreen(), desc='Toggle fullscreen'),
+keys_assignation = [
+    # Switch between windows
+    ([mod], ['s', 'Down'], lazy.layout.down(),
+        'Move focus down in stack pane'),
+    ([mod], ['r', 'Up'], lazy.layout.up(),
+        'Move focus up in stack pane'),
+    ([mod], ['t', 'Left'], lazy.layout.left(),
+        'Move focus left in stack pane'),
+    ([mod], ['n', 'Right'], lazy.layout.right(),
+        'Move focus right in stack pane'),
+
+    # Move windows
+    ([mod, 'shift'], 's', lazy.layout.shuffle_down(),
+        'Move window down in current stack '),
+    ([mod, 'shift'], 'r', lazy.layout.shuffle_up(),
+        'Move window up in current stack '),
+    ([mod, 'shift'], 't', lazy.layout.shuffle_left(),
+        'Move window left in current stack '),
+    ([mod, 'shift'], 'n', lazy.layout.shuffle_right(),
+        'Move window right in current stack '),
+
+    # Windows geometry
+    ([mod], 'd', [lazy.layout.shrink(), lazy.layout.decrease_nmaster()],
+        'Shrink window (MonadTall), decrease number in master pane (Tile)'),
+    ([mod], 'l', [lazy.layout.grow(), lazy.layout.increase_nmaster()],
+        'Expand window (MonadTall), increase number in master pane (Tile)'),
+    ([mod], 'v', lazy.layout.maximize(), 'Maximize'),
+    ([mod], 'f', lazy.window.toggle_fullscreen(), 'Toggle fullscreen'),
 
     # Switch window focus to other pane(s) of stack
-    Key([mod], 'space', lazy.layout.next(),
-        desc='Switch window focus to other pane(s) of stack'),
+    ([mod], 'space', lazy.layout.next(),
+        'Switch window focus to other pane(s) of stack'),
 
     # Swap panes of split stack
-    Key([mod, 'shift'], 'space', lazy.layout.rotate(),
-        desc='Swap panes of split stack'),
+    ([mod, 'shift'], 'space', lazy.layout.rotate(),
+        'Swap panes of split stack'),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, 'shift'], 'Return', lazy.layout.toggle_split(),
-        desc='Toggle between split and unsplit sides of stack'),
-    Key([mod], 'Return', lazy.spawn(terminal), desc='Launch terminal'),
+    ## Toggle between split and unsplit sides of stack.
+    ## Split = all windows displayed
+    ## Unsplit = 1 window displayed, like Max layout, but still with
+    ## multiple stack panes
+    ([mod, 'shift'], 'Return', lazy.layout.toggle_split(),
+        'Toggle between split and unsplit sides of stack'),
+    ([mod], 'Return', lazy.spawn(terminal), 'Launch terminal'),
 
-    # Toggle between different layouts as defined below
-    Key([mod], 'Tab', lazy.next_layout(), desc='Toggle between layouts'),
-    Key([mod], 'w', lazy.window.kill(), desc='Kill focused window'),
+    # Toggle between different layouts
+    ([mod], 'Tab', lazy.next_layout(), 'Toggle between layouts'),
+    ([mod], 'w', lazy.window.kill(), 'Kill focused window'),
 
-    Key([mod, 'control'], 'r', lazy.restart(), desc='Restart qtile'),
-    Key([mod, 'control'], 'q', lazy.shutdown(), desc='Shutdown qtile'),
+    # Qtile session
+    ([mod, 'control'], 'r', lazy.restart(), 'Restart qtile'),
+    ([mod, 'control'], 'q', lazy.shutdown(), 'Shutdown qtile'),
 
-    # Key([alt], 'space', lazy.spawncmd(),
-    #     desc='Spawn a command using a prompt widget'),
-    Key([alt], 'space', lazy.spawn('dmenu_run -p Run: -l 5 -sb dimgrey'),
-        desc='Spawn a command using a prompt widget'),
+    # Run prompt
+    # ([alt], 'space', lazy.spawncmd(),
+    #     'Spawn a command using a prompt widget'),
+    ([alt], 'space', lazy.spawn('dmenu_run -p Run: -l 5 -sb dimgrey'),
+        'Spawn a command using a prompt widget'),
 
     # Switch between groups
-    Key([alt, 'control'], 'Left', lazy.screen.prev_group(),
-        desc='Move to the group on the left'),
-    Key([alt, 'control'], 'Right', lazy.screen.next_group(),
-        desc='Move to the group on the right'),
+    ([alt, 'control'], 'Left', lazy.screen.prev_group(),
+        'Move to the group on the left'),
+    ([alt, 'control'], 'Right', lazy.screen.next_group(),
+        'Move to the group on the right'),
 
     # Sound
-    Key([], 'XF86AudioMute', lazy.spawn('amixer -q set Master toggle')),
-    Key([], 'XF86AudioLowerVolume',
-        lazy.spawn('amixer -c 0 sset Master 1- unmute')),
-    Key([], 'XF86AudioRaiseVolume',
-        lazy.spawn('amixer -c 0 sset Master 1+ unmute')),
+    ([], 'XF86AudioMute', lazy.spawn('amixer -q set Master toggle'), ''),
+    ([], 'XF86AudioLowerVolume',
+        lazy.spawn('amixer -c 0 sset Master 1- unmute'), ''),
+    ([], 'XF86AudioRaiseVolume',
+        lazy.spawn('amixer -c 0 sset Master 1+ unmute'), ''),
 
     # Brightness
-    Key([], 'XF86MonBrightnessDown', lazy.spawn('xbacklight -dec 15')),
-    Key([], 'XF86MonBrightnessUp', lazy.spawn('xbacklight -inc 15')),
+    ([], 'XF86MonBrightnessDown', lazy.spawn('xbacklight -dec 15'), ''),
+    ([], 'XF86MonBrightnessUp', lazy.spawn('xbacklight -inc 15'), ''),
 ]
+
+for modifiers, key, commands, desc in keys_assignation:
+    if not isinstance(commands, (tuple, list)):
+        commands = [commands]
+    if isinstance(key, (tuple, list)):
+        assign_multiple_keys(keys, modifiers, key, *commands, desc='')
+    else:
+        keys.append(Key(modifiers, key, *commands, desc=desc))
+
 
 group_names = [
     ('1', 'quotedbl'),
