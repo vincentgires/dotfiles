068f4376177913e37e6861cc7852a6ab0fb100e6
Vincent Girès <mail@vincentgires.com>
2020-12-03 11:07:27 +0100

    Toggle floating on mod ctrl left clic

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -332,7 +332,8 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([mod], 'Button3', lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], 'Button2', lazy.window.bring_to_front())
+    Click([mod], 'Button2', lazy.window.bring_to_front()),
+    Click([mod, 'control'], 'Button1', lazy.window.toggle_floating())
 ]
 
 dgroups_key_binder = None
