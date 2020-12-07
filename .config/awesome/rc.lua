9de1dd44aac3de900a0fd68ea4ac39cbac647d74
Vincent Girès <mail@vincentgires.com>
2020-12-08 00:56:52 +0100

    Set Blender rule
    Set Blender to not start as maximize which is the default
    on this application.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -578,6 +578,14 @@ awful.rules.rules = {
   -- Set Firefox to always map on the tag named '2' on screen 1
   -- {rule = { class = 'Firefox'},
   --  properties = {screen = 1, tag = '2'}},
+
+  -- Set Blender to not start as maximize which is the default on this application
+  {
+    rule_any = {
+      class = {'Blender'},
+    },
+    properties = {maximized = false, minimized = true}
+  },
 }
 -- }}}
 
