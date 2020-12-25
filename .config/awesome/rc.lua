3978c34c88f8a0616fd246441ed3a5280b494e9c
Vincent Girès <mail@vincentgires.com>
2020-12-26 00:20:19 +0100

    Make dialogs ontop
    Make dialogs ontop to avoid losing them behind tiled windows

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -639,6 +639,12 @@ awful.rules.rules = {
     properties = {titlebars_enabled = true}
   },
 
+  -- Make dialogs ontop to avoid losing them behind tiled windows
+  {
+    rule_any = {type = {'dialog'}},
+    properties = {ontop = true}
+  },
+
   -- Set Firefox to always map on the tag named '2' on screen 1
   -- {rule = { class = 'Firefox'},
   --  properties = {screen = 1, tag = '2'}},
