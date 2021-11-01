426c82cc97d1206d8a0356f687a69c8eae0b5b1c
Vincent Girès <mail@vincentgires.com>
2021-11-01 21:45:16 +0100

    Update xrandr command

--- .local/bin/xrandr_to_lvds1.sh
+++ .local/bin/xrandr_to_lvds1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VGA1 --off --output VIRTUAL1 --off
+xrandr --output LVDS-1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off
