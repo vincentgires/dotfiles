0a793b18e813d6954a615b5eea991bc4e885c347
Vincent Girès <mail@vincentgires.com>
2023-03-30 16:00:37 +0200

    Clean imports

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,6 +1,5 @@
-import os
 import subprocess
-from libqtile import bar, layout, widget, hook
+from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from qtile_cfg import (
