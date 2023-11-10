c9870200eee3defa827b9535b432a81ae7fb73b5
Vincent Girès <vincent@vincent.bio>
2023-11-10 14:23:49 +0100

    Add audacious in floating mode

--- .config/awesome/awesome_cfg.lua
+++ .config/awesome/awesome_cfg.lua
@@ -7,7 +7,7 @@ config = {
      properties = {tag = 'dev'}},
     {rule_any = {class = {'Element', 'discord'}},
      properties = {tag = 'chat'}},
-    {rule_any = {class = {'mpv', 'vlc', 'QjackCtl'}},
+    {rule_any = {class = {'mpv', 'vlc', 'Audacious', 'QjackCtl'}},
      properties = {floating = true, ontop = true}},
     {rule = {class = 'Blender'},
      properties = {maximized = false}}
